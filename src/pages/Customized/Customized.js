e30e4cacbe775944ddaf9dd9d2fdfec8560a20fd
vivcyber <mspinksun@gmail.com>
2022-07-14 17:46:28 +0800

    客製化ㄉ背景喔～

--- src/pages/Customized/Customized.js
+++ src/pages/Customized/Customized.js
@@ -4,12 +4,16 @@ import './Customized.css'
 const Customized = () => {
     return (
         <>
-        <div className="cus_mask w-100 vh-100 d-flex justify-content-end align-items-end">
-          <div className=" work-area col-10 pb-5 pe-5 text-white bg-dark overflow-hidden">
-          <div className='bg-danger w-100 h-100'>
+        <div className="w-100 vh-100 d-flex justify-content-end align-items-end">
+        <div className='cus_matte w-100 vh-100 '>
+            <img src='./imgs/cus_bg_04.png'/>
+        </div>
+
+          <div className=" work-area col-10 pb-5 pe-5 text-danger">
+         
           大家製作放這邊～～～～
 
-          </div>
+         
           {/* <img src='./imgs/a.jpg' /> */}
 
            
