ec7867dd18d7ec619bf02258bd8a5ef24ea1b9e4
cooler0713 <qqoogg00@yahoo.com.tw>
2022-07-14 19:40:46 +0800

    '調整lesson排版'

--- src/pages/Customized/Customized.js
+++ src/pages/Customized/Customized.js
@@ -1,26 +1,21 @@
 import React from 'react';
-import './Customized.css'
+import './Customized.css';
 
 const Customized = () => {
-    return (
-        <>
-        <div className="w-100 vh-100 d-flex justify-content-end align-items-end">
-        <div className='cus_matte w-100 vh-100 '>
-            <img src='./imgs/cus_bg_04.png'/>
+  return (
+    <>
+      <div className="w-100 vh-100 d-flex justify-content-end align-items-end">
+        <div className="cus_matte w-100 vh-100 ">
+          <img src="./imgs/cus_bg_04.png" />
         </div>
 
-          <div className=" work-area col-10 pb-5 pe-5 text-danger">
-         
+        <div className=" work-area col-10 pb-5 pe-5 text-danger">
           大家製作放這邊～～～～
-
-         
           {/* <img src='./imgs/a.jpg' /> */}
-
-           
-          </div>
         </div>
-      </>
-    )
+      </div>
+    </>
+  );
 };
 
 export default Customized;
