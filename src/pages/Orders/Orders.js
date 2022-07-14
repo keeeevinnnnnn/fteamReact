e3e61fec17e18da2ddbef930569c64f52dc4dbd5
NathanLai <a403440322@gmail.com>
2022-07-14 17:49:48 +0800

    order update merge

--- src/pages/Orders/Orders.js
+++ src/pages/Orders/Orders.js
@@ -1,11 +1,10 @@
 import React from 'react';
+import '../Orders/Orders.css';
 const Orders = () => {
   return (
     <>
       <div className="od-bg w-100 vh-100 d-flex justify-content-end align-items-end">
-        <div className="work-area col-10 p-0 text-danger">
-          大家製作放這邊～～～～
-        </div>
+        <div className="work-area col-10 p-0"></div>
       </div>
     </>
   );
