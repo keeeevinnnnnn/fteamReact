6b287097abf5fbbd506a7d129f1e0d4347d9b433
Gary <garylin0969@gmail.com>
2022-07-17 17:05:20 +0800

    0717

--- src/pages/Login/components/LoginCariusel.js
+++ src/pages/Login/components/LoginCariusel.js
@@ -1,55 +1,48 @@
 import React from 'react';
-// 輪播牆
-import 'react-responsive-carousel/lib/styles/carousel.min.css';
-import { Carousel } from 'react-responsive-carousel';
+import { Autoplay } from 'swiper';
+import { EffectCube } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css/bundle';
+
+// 圖片陣列 方便map用
+const datas = [
+  { src: '../../imgs/Logoin/Rectangle.png' },
+  { src: '../../imgs/Logoin/cfbcff9f1af9abea43111d2f52f342fc.jpg' },
+  { src: '../../imgs/Logoin/cf8d25ed01695253f9e3a81ecc2a108b.jpg' },
+];
 
 const LoginCariusel = () => {
   return (
     <>
       <div className="col-xl-6 h-100 LoginCarousel">
-        <Carousel
-          thumbWidth="0px"
-          infiniteLoop="true"
-          autoPlay="true"
-          interval="3000"
-          axis="horizontal"
-          stopOnHover=""
-          showThumbs=""
-          showIndicators=""
-          showStatus=""
-          showArrows=""
+        <Swiper
+          speed={3000}
+          grabCursor={true}
+          loop={true}
+          spaceBetween={0}
+          slidesPerView={1}
+          modules={[EffectCube, Autoplay]}
+          effect="cube"
+          autoplay={{
+            delay: 1000,
+            disableOnInteraction: false,
+            pauseOnMouseEnter: true,
+          }}
         >
-          <div>
-            <img
-              style={{
-                objectFit: 'cover',
-                height: 'calc(100vh - 88px)',
-              }}
-              src="../../imgs/Logoin/Rectangle.png"
-              alt=""
-            />
-          </div>
-          <div>
-            <img
-              style={{
-                objectFit: 'cover',
-                height: 'calc(100vh - 88px)',
-              }}
-              src="../../imgs/Logoin/588e6470267c297a2478709e0e788a95.jpg"
-              alt=""
-            />
-          </div>
-          <div>
-            <img
-              style={{
-                objectFit: 'cover',
-                height: 'calc(100vh - 88px)',
-              }}
-              src="../../imgs/Logoin/cf8d25ed01695253f9e3a81ecc2a108b.jpg"
-              alt=""
-            />
-          </div>
-        </Carousel>
+          {datas.map((data) => (
+            <SwiperSlide key={data.src}>
+              <img
+                style={{
+                  objectFit: 'cover',
+                  height: 'calc(100vh - 88px)',
+                  width: '100%',
+                }}
+                src={data.src}
+                alt=""
+              />
+            </SwiperSlide>
+          ))}
+        </Swiper>
       </div>
     </>
   );
