c1078cf97f0c61c2b2088d227527ac4af9b6f343
kevin wu <kevin85627@gmail.com>
2022-07-21 23:06:29 +0800

    123

--- src/pages/Product/ProductMain.js
+++ src/pages/Product/ProductMain.js
@@ -4,41 +4,56 @@ import ToolBox from './components/ToolBox';
 import './styles/ProductMain.scss';
 import axios from './commons/axios';
 import ProductList from './components/ProductList';
+import { Link, useLocation } from 'react-router-dom';
+import Pagination from './components/Pagination';
 const ProductMain = () => {
   const [data, setData] = useState({});
-  const getData = async () => {
-    axios.get('/product').then((res) => {
+  const location = useLocation();
+  const usp = new URLSearchParams(location.search);
+  const getPageData = async (event, gotoPage) => {
+    if (event) {
+      event.preventDefault();
+    }
+    console.log({ gotoPage });
+
+    axios.get(`/product?page=${gotoPage}`).then((res) => {
       setData(res.data);
     });
   };
 
   useEffect(() => {
-    getData();
-  }, []);
+    getPageData(null, +usp.get('page') || 1);
+  }, [location]);
   return (
-    <>
-      <div className="w-100 vh-100 d-flex justify-content-end align-items-end">
-        <div className="work-area col-10 pb-5 pe-5 text-danger">
-          <ToolBox />
-          <FilterBox />
-          <div className="row product-list">
-            {data && data.rows
-              ? data.rows.map((r) => {
-                  return (
-                    <ProductList
-                      sid={r.sid}
-                      img={r.img}
-                      name={r.name}
-                      brand={r.brand}
-                      price={r.price}
-                    />
-                  );
-                })
-              : null}
+    <div className="w-100 vh-100 d-flex justify-content-end align-items-end">
+      <div className="work-area col-10 pb-5 pe-5 text-danger">
+        <ToolBox />
+        <FilterBox />
+        <div className="row product-list">
+          {data && data.rows
+            ? data.rows.map((r) => {
+                return (
+                  <ProductList
+                    sid={r.sid}
+                    img={r.img}
+                    name={r.name}
+                    brand={r.brand}
+                    price={r.price}
+                  />
+                );
+              })
+            : null}
+        </div>
+
+        <div className="row paginationBox">
+          <div className="col-4">
+            {data && data.totalPages ? (
+              <Pagination page={data.page} totalPages={data.totalPages} />
+            ) : null}
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
--- src/pages/Product/components/ToolBox.js
+++ src/pages/Product/components/ToolBox.js
@@ -1,31 +1,31 @@
 import React, { useState } from 'react';
 
 const ToolBox = () => {
-    const menuItems = ['Skateboard', 'Decks', 'Trucks', 'Wheels'];
-    const [menu, setMenu] = useState('Skateboard');
-    return (
-        <>
-            <div className="row toolbox">
-                <div className="col-4">
-                    <ul>
-                        {menuItems.map((v, i) => {
-                            return (
-                                <li
-                                    key={i}
-                                    onClick={() => {
-                                        setMenu(v);
-                                    }}
-                                    className={menu === v ? 'catgory' : ''}
-                                >
-                                    <a href="#/">{v}</a>
-                                </li>
-                            );
-                        })}
-                    </ul>
-                </div>
-            </div>
-        </>
-    );
+  const menuItems = ['Skateboard', 'Decks', 'Trucks', 'Wheels'];
+  const [menu, setMenu] = useState('Skateboard');
+  return (
+    <>
+      <div className="row toolbox">
+        <div className="col-4">
+          <ul>
+            {menuItems.map((v, i) => {
+              return (
+                <li
+                  key={i}
+                  onClick={() => {
+                    setMenu(v);
+                  }}
+                  className={menu === v ? 'catgory' : ''}
+                >
+                  <a href="#/">{v}</a>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default ToolBox;
