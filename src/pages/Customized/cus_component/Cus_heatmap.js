d60f896ac602ae2354697b689ee40acbe796060d
vivcyber <mspinksun@gmail.com>
2022-07-22 00:06:40 +0800

    94想放github月曆

---
+++ src/pages/Customized/cus_component/Cus_heatmap.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import CalendarHeatmap from 'react-calendar-heatmap';
+import 'react-calendar-heatmap/dist/styles.css';
+
+function Cus_heatmap() {
+  return (
+    <>
+      <CalendarHeatmap 
+        startDate={new Date('2022-01-01')}
+        endDate={new Date('2022-08-30')}
+        values={[
+          { date: '2022-01-02', count: 1 },
+          { date: '2022-02-03', count: 4 },
+          { date: '2022-02-04', count: 4 },
+          { date: '2022-02-07', count: 4 },
+          { date: '2022-02-022', count: 4 },
+          { date: '2022-03-15', count: 3 },
+          { date: '2022-03-17', count: 3 },
+          { date: '2022-03-18', count: 3 },
+          { date: '2022-03-21', count: 3 },
+          { date: '2022-03-27', count: 3 },
+          { date: '2022-03-30', count: 3 },
+          { date: '2022-04-01', count: 3 },
+          { date: '2022-04-05', count: 3 },
+          { date: '2022-04-08', count: 3 }, 
+          { date: '2022-04-09', count: 3 },
+          { date: '2022-05-20', count: 6 },
+          { date: '2022-07-02', count: 1 },
+          { date: '2022-07-03', count: 4 },
+          { date: '2022-07-15', count: 3 },
+          { date: '2022-07-20', count: 6 },
+          // ...and so on
+        ]}
+      />
+    </>
+  );
+}
+
+export default Cus_heatmap;
