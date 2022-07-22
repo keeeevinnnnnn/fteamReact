9971267cd0832aca3c662d216bff9f2f1577e75b
vivcyber <mspinksun@gmail.com>
2022-07-23 00:26:06 +0800

    抖抖抖抖

--- src/pages/Customized/Cus_product_detail.js
+++ src/pages/Customized/Cus_product_detail.js
@@ -10,7 +10,7 @@ function Cus_product_detail() {
     <>
       <div className="w-100 vh-100 d-flex justify-content-end align-items-end">
         <div className="work-area col-12 col-md-10 p-0">
-          <div class="row px-5">
+          <div class="d-flex px-5">
             <div className="left col-12 col-sm-6 ">
               <p>Project Name</p>
               <h2>My Cool Board</h2>
