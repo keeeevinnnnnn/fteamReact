cf5be96def75c0f2ccee6df0340ba0ed1defaff8
vivcyber <mspinksun@gmail.com>
2022-07-22 23:04:13 +0800

    新增喔

--- src/pages/Customized/Customized_explore.js
+++ src/pages/Customized/Customized_explore.js
@@ -1,21 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Share_card from './cus_component/Share_card';
 
 function Customized_explore() {
   return (
     <>
       <div className="w-100 vh-100 d-flex justify-content-end align-items-end">
         <div className="work-area col-12 col-md-10 p-0">
-          大概是分享牆喔
-
-          <div className="row">
-            <div className="card col-12 col-sm-4">
-              <h3>Prject Name</h3>
-              有商品再來map
-              <Link to={'/customized/create/detail'}>
-                <button className="btn btn-dark">View</button>
-              </Link>
-            </div>
+          <div className="d-flex col-12 col-mb-4 flex-wrap w-100 d-flex justify-content-center">
+          <Share_card/>
+          <Share_card/>
+          <Share_card/>
+          <Share_card/>
+          <Share_card/>
+          <Share_card/>
+           
           </div>
         </div>
       </div>
