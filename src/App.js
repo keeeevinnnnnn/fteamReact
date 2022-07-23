df10a359e2de39b23a2833a1a153728c1e9a10e9
cooler0713 <qqoogg00@yahoo.com.tw>
2022-07-23 14:05:10 +0800

    修改

--- src/App.js
+++ src/App.js
@@ -29,126 +29,101 @@ import AuthContextProvider from './components/AuthContextProvider';
 // export const AuthContext = React.createContext();
 export const MemberContext = React.createContext();
 function App() {
-    // 登入狀態判斷
-    // const [auth, setAuth] = useState(false);
-    // 把上述State包成物件Context下去
-    // const authObj = {
-    //     auth: auth,
-    //     setAuth: setAuth,
-    // };
-    // 從Nav接住登入會員的個人資料
-    const [member, setMember] = useState([]);
-    // 把上述State包成物件Context下去
-    const memberObj = {
-        member: member,
-        setMember: setMember,
-    };
-    // 登入後會存的值user_token JWT
-    // const token = localStorage.getItem('user_token');
-    return (
-        <Router>
-            <AuthContextProvider>
-                {/* 所有人都可以直接拿到token */}
-                {/* <TokenContext.Provider value={token}> */}
-                {/* 會員個人資料 */}
-                <MemberContext.Provider value={memberObj}>
-                    {/* 把登入狀態傳下去 */}
-                    {/* <AuthContext.Provider value={authObj}> */}
-                    <div className="container-fluid vh-100 fteam-wrap">
-                        <div className="row h-100">
-                            <div className="p-0 d-flex flex-column w-100 h-100">
-                                <Navbar />
-                                <div className="w-100 bottom-grid d-flex">
-                                    <SideBar />
+  // 登入狀態判斷
+  // const [auth, setAuth] = useState(false);
+  // 把上述State包成物件Context下去
+  // const authObj = {
+  //     auth: auth,
+  //     setAuth: setAuth,
+  // };
+  // 從Nav接住登入會員的個人資料
+  const [member, setMember] = useState([]);
+  // 把上述State包成物件Context下去
+  const memberObj = {
+    member: member,
+    setMember: setMember,
+  };
+  // 登入後會存的值user_token JWT
+  // const token = localStorage.getItem('user_token');
+  return (
+    <Router>
+      <AuthContextProvider>
+        {/* 所有人都可以直接拿到token */}
+        {/* <TokenContext.Provider value={token}> */}
+        {/* 會員個人資料 */}
+        <MemberContext.Provider value={memberObj}>
+          {/* 把登入狀態傳下去 */}
+          {/* <AuthContext.Provider value={authObj}> */}
+          <div className="container-fluid vh-100 fteam-wrap">
+            <div className="row h-100">
+              <div className="p-0 d-flex flex-column w-100 h-100">
+                <Navbar />
+                <div className="w-100 bottom-grid d-flex">
+                  <SideBar />
 
-                                    {/* col-2 的側邊欄 */}
-                                    {/* <div className="col-10 h-100 p-0 "></div>有要加sidebar的 用col-10 這段 沒有的用 col-12 */}
+                  {/* col-2 的側邊欄 */}
+                  {/* <div className="col-10 h-100 p-0 "></div>有要加sidebar的 用col-10 這段 沒有的用 col-12 */}
 
-                                    <Routes>
-                                        <Route path="/" element={<Home />} />
-                                        <Route
-                                            path="/member"
-                                            element={<Member />}
-                                        />
-                                        <Route
-                                            path="/login"
-                                            element={<Login />}
-                                        />
-                                        <Route
-                                            path="/lesson"
-                                            element={<Lesson />}
-                                        />
-                                        <Route
-                                            path="/lesson/lesson_zhongxiao"
-                                            element={<Lesson_zhongxiao />}
-                                        />
-                                        <Route
-                                            path="/lesson/lesson_banqiao"
-                                            element={<Lesson_banqiao />}
-                                        />
-                                        <Route
-                                            path="/products"
-                                            element={<ProductMain />}
-                                        />
-                                        <Route
-                                            path="/customized"
-                                            element={<Customized />}
-                                        />
-                                        <Route
-                                            path="/customized/create"
-                                            element={<Customized_add />}
-                                        />
-                                        <Route
-                                            path="/customized/create/wheel"
-                                            element={<Cus_product_card_wheel />}
-                                        />
-                                        <Route
-                                            path="/customized/create/carrier"
-                                            element={
-                                                <Cus_product_card_struct />
-                                            }
-                                        />
-                                        <Route
-                                            path="/customized/create/front_deck"
-                                            element={
-                                                <Cus_product_card_fcolor />
-                                            }
-                                        />
-                                        <Route
-                                            path="/customized/create/back"
-                                            element={<Cus_product_card_back />}
-                                        />
-                                        <Route
-                                            path="/customized/create/detail"
-                                            element={<Cus_product_detail />}
-                                        />
-                                        <Route
-                                            path="/customized/explore"
-                                            element={<Customized_explore />}
-                                        />
-                                        <Route
-                                            path="/customized/collect"
-                                            element={<Customized_collect />}
-                                        />
-                                        <Route
-                                            path="/carts"
-                                            element={<Carts />}
-                                        />
-                                        <Route
-                                            path="/orders"
-                                            element={<Orders />}
-                                        />
-                                    </Routes>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    {/* </AuthContext.Provider> */}
-                </MemberContext.Provider>
-                {/* </TokenContext.Provider> */}
-            </AuthContextProvider>
-        </Router>
-    );
+                  <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/member" element={<Member />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/lesson" element={<Lesson />} />
+                    <Route
+                      path="/lesson/lesson_zhongxiao"
+                      element={<Lesson_zhongxiao />}
+                    />
+                    <Route
+                      path="/lesson/lesson_banqiao"
+                      element={<Lesson_banqiao />}
+                    />
+                    <Route path="/products" element={<ProductMain />} />
+                    <Route path="/customized" element={<Customized />} />
+                    <Route
+                      path="/customized/create"
+                      element={<Customized_add />}
+                    />
+                    <Route
+                      path="/customized/create/wheel"
+                      element={<Cus_product_card_wheel />}
+                    />
+                    <Route
+                      path="/customized/create/carrier"
+                      element={<Cus_product_card_struct />}
+                    />
+                    <Route
+                      path="/customized/create/front_deck"
+                      element={<Cus_product_card_fcolor />}
+                    />
+                    <Route
+                      path="/customized/create/back"
+                      element={<Cus_product_card_back />}
+                    />
+                    <Route
+                      path="/customized/create/detail"
+                      element={<Cus_product_detail />}
+                    />
+                    <Route
+                      path="/customized/explore"
+                      element={<Customized_explore />}
+                    />
+                    <Route
+                      path="/customized/collect"
+                      element={<Customized_collect />}
+                    />
+                    <Route path="/carts" element={<Carts />} />
+                    <Route path="/orders" element={<Orders />} />
+                  </Routes>
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* </AuthContext.Provider> */}
+        </MemberContext.Provider>
+        {/* </TokenContext.Provider> */}
+      </AuthContextProvider>
+    </Router>
+  );
 }
 
 export default App;
