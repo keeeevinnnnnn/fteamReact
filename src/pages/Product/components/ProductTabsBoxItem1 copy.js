ba6a9f8dd6dee79ff284029c2695e0a690566fa0
kevin wu <kevin85627@gmail.com>
2022-07-24 16:33:35 +0800

    123

--- src/pages/Product/components/ProductTabsBoxItem1 copy.js
+++ src/pages/Product/components/ProductTabsBoxItem1 copy.js
@@ -4,35 +4,12 @@ import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 
 const ProductTabsBoxItem1 = (props) => {
-  function SampleNextArrow(props) {
-    const { className, style, onClick } = props;
-    return (
-      <div
-        className={className}
-        style={{ ...style, display: 'block' }}
-        onClick={onClick}
-      />
-    );
-  }
-
-  function SamplePrevArrow(props) {
-    const { className, style, onClick } = props;
-    return (
-      <div
-        className={className}
-        style={{ ...style, display: 'block' }}
-        onClick={onClick}
-      />
-    );
-  }
   const settings = {
     dots: true,
     infinite: true,
     speed: 500,
     slidesToShow: 4,
     slidesToScroll: 1,
-    nextArrow: <SampleNextArrow />,
-    prevArrow: <SamplePrevArrow />,
   };
   return (
     <>
