f9306a1732f2ed7ff0310af4caddb7e7ee6a174d
Gary <garylin0969@gmail.com>
2022-07-24 21:38:20 +0800

    0724

--- src/pages/Member/components/Information/AvatarForm.js
+++ src/pages/Member/components/Information/AvatarForm.js
@@ -1,18 +1,23 @@
-import React, { useRef, useState } from 'react';
+import React, { useRef, useState, useContext } from 'react';
 import axios from 'axios';
+import AuthContext from '../../../../components/AuthContext';
 import '../../styles/AvatarForm.scss';
+import { v4 as uuidv4 } from 'uuid';
 
 const AvatarForm = ({ member }) => {
+  // token 發fetch用 setAuths更改狀態重新撈取會員資料
+  const { token, auths, setAuths } = useContext(AuthContext);
+
   // 取得頭貼input
   const registerAvatarRef = useRef(null);
-
-  // 記錄表單每個欄位輸入值
-  const [field, setFields] = useState(member.mem_avatar);
 
   // 點擊頭貼 觸發頭貼input
   function clickAvatar() {
     registerAvatarRef.current.click();
   }
+
+  // 記錄頭貼檔案名稱
+  const [field, setField] = useState(member.mem_avatar);
 
   // 頭貼input值有變換時
   async function uploadAvatar(e) {
@@ -22,29 +27,36 @@ const AvatarForm = ({ member }) => {
       'http://localhost:3000/member/upload',
       data
     );
-    setFields(response.data.filename);
+    setField(response.data.filename);
   }
 
   // 表單點擊送出後
   async function handleSubmit(e) {
     // 先阻擋預設送出行為
     e.preventDefault();
-
+    console.log(field);
     const response = await axios.post(
-      'http://localhost:3000/member/register',
-      // fields
+      'http://localhost:3000/member/avatar',
+      { avatar: field },
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
     );
 
     if (response.data.success === true) {
-      alert('註冊成功');
+      setAuths({ ...auths, change: uuidv4() });
+      alert('頭貼修改成功');
     } else {
-      alert('註冊失敗');
+      alert('頭貼修改失敗');
     }
   }
   return (
     <>
       <form
-        className="h-100 memberAvatarForm" // 表單點擊
+        className="h-100 memberAvatarForm"
+        // 表單點擊
         onSubmit={handleSubmit}
       >
         <input
@@ -54,7 +66,6 @@ const AvatarForm = ({ member }) => {
           onChange={uploadAvatar}
           hidden
         />
-        {/* 頭貼上傳表單 */}
         <figure className="d-flex justify-content-center h-70">
           <img
             src={`http://localhost:3000/avatar/${field}`}
