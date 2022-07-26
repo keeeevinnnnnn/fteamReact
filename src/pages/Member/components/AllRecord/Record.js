4d05fb5d1a859d83b21f91a3ab732d5efd38918b
Gary <garylin0969@gmail.com>
2022-07-27 03:03:42 +0800

    0727

--- src/pages/Member/components/AllRecord/Record.js
+++ src/pages/Member/components/AllRecord/Record.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ScrollBox from '../../../../components/ScrollBox/ScrollBox';
 import RecordCard from './RecordCard';
 import RecordEcharts from './RecordEcharts';
@@ -7,14 +7,39 @@ import '../../styles/Record.scss';
 
 const Record = () => {
   // 下拉選單
+  const [select, setSelect] = useState('All');
   const option = ['All', 'Products', 'Customized'];
+  let products = '';
+  let customized = '';
+
+  if (select === 'Products') {
+    products = '';
+  } else {
+    products = 'd-none';
+  }
+  if (select === 'Customized') {
+    customized = '';
+  } else {
+    customized = 'd-none';
+  }
+  if (select === 'All') {
+    products = '';
+    customized = '';
+  }
+
+  console.log(select);
   return (
     <>
       <div className="w-100 h-100  d-flex flex-warp">
         <div className="col-12 col-xl-6 h-100">
           <div className="d-flex justify-content-around">
             <section id="select">
-              <select>
+              <select
+                value={select}
+                onChange={(e) => {
+                  setSelect(e.target.value);
+                }}
+              >
                 {option.map((v, i) => {
                   return (
                     <option key={uuidv4()} value={v}>
@@ -26,7 +51,7 @@ const Record = () => {
             </section>
           </div>
           <ScrollBox>
-            <RecordCard />
+            <RecordCard products={products} customized={customized} />
           </ScrollBox>
         </div>
 
--- src/pages/Member/components/AllRecord/RecordCard.js
+++ src/pages/Member/components/AllRecord/RecordCard.js
@@ -4,16 +4,18 @@ import axios from 'axios';
 import { v4 as uuidv4 } from 'uuid';
 import '../../styles/RecordCard.scss';
 
-const RecordCard = () => {
+const RecordCard = ({ products, customized }) => {
   const { auth, token, logout, auths } = useContext(AuthContext);
   const [recordProducts, setRecordProducts] = useState([]);
+  const [recordCustomized, setRecordCustomized] = useState([]);
 
+  // 商品
   useEffect(() => {
     if (!auth) {
       return;
     } else {
       axios
-        .get('http://localhost:3000/member/record', {
+        .get('http://localhost:3000/member/recordproducts', {
           // 發JWT一定要加這個headers
           headers: {
             Authorization: `Bearer ${token}`,
@@ -22,14 +24,25 @@ const RecordCard = () => {
         .then((res) => {
           setRecordProducts(res.data);
         });
+      axios
+        .get('http://localhost:3000/member/recordcustomized', {
+          // 發JWT一定要加這個headers
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        })
+        .then((res) => {
+          setRecordCustomized(res.data);
+        });
     }
   }, []);
-  // console.log(Object.keys(recordProducts));
+
   return (
     <>
+      {/* 商品 */}
       {Object.values(recordProducts).map((v, i) => {
         return (
-          <div className="w-95 m-3 recordCard" key={uuidv4()}>
+          <div className={`w-95 m-3 recordCard ${products}`} key={uuidv4()}>
             <div className="d-flex h-100 w-100">
               <div className="col-xl-3">
                 <img
@@ -44,6 +57,34 @@ const RecordCard = () => {
               </div>
               <div className="col-xl-9">
                 <p className="h-50 p-3">{v.name}</p>
+                <div className="h-50 d-flex justify-content-around">
+                  <h6>{v.order_date}</h6>
+                  <h5>$ {v.price}</h5>
+                </div>
+              </div>
+            </div>
+          </div>
+        );
+      })}
+
+      {/* 客製化商品 */}
+      {Object.values(recordCustomized).map((v, i) => {
+        return (
+          <div className={`w-95 m-3 recordCard ${customized}`} key={uuidv4()}>
+            <div className="d-flex h-100 w-100">
+              <div className="col-xl-3">
+                <img
+                  src={`http://localhost:3000/custom/${v.img}`}
+                  alt=""
+                  className="h-100"
+                  style={{
+                    objectFit: 'cover',
+                    aspectRatio: '1/1',
+                  }}
+                />
+              </div>
+              <div className="col-xl-9">
+                <p className="h-50 p-3">{v.custom_product_name}</p>
                 <div className="h-50 d-flex justify-content-around">
                   <h6>{v.order_date}</h6>
                   <h5>$ {v.price}</h5>
