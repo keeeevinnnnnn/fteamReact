8d13a610700dabe126afe8d9f9fce1966b52e7ed
GaryL1n <106055223+GaryL1n@users.noreply.github.com>
2022-07-28 15:49:14 +0800

    0728

---
+++ src/pages/Member/components/AllRecord/Lesson.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ScrollBox from '../../../../components/ScrollBox/ScrollBox';
+import LessonCard from './LessonCard';
+
+const Lesson = () => {
+  return (
+    <>
+      <ScrollBox>
+        <LessonCard />
+        <LessonCard />
+        <LessonCard />
+        <LessonCard />
+        <LessonCard />
+        <LessonCard />
+        <LessonCard />
+        <LessonCard />
+        <LessonCard />
+      </ScrollBox>
+    </>
+  );
+};
+
+export default Lesson;
--- src/pages/Member/components/AllRecord/LessonCard.js
+++ src/pages/Member/components/AllRecord/LessonCard.js
@@ -4,8 +4,8 @@ import '../../styles/LessonCard.scss';
 const LessonCard = () => {
   return (
     <>
-      <div className="h-40 bg-light m-2">
-        <div className="d-flex align-items-center text-center h-25 bg-info">
+      <div className="m-2 lessonCard m-3">
+        <div className="d-flex align-items-center text-center lessonhead">
           <p className="col-4 m-0">Collapse - Eminem / Baiba Klints</p>
           <p className="col-4 m-0">
             <span>Begin</span> 2022-09-25
@@ -14,8 +14,25 @@ const LessonCard = () => {
             <span>End</span> 2022-10-25
           </p>
         </div>
-        <div>
-          
+        <div className="d-flex justify-content-center align-items-center lessonbody">
+          <table class="table w-90">
+            <thead>
+              <tr>
+                <th scope="col">Teacher</th>
+                <th scope="col">Location</th>
+                <th scope="col">Quota</th>
+                <th scope="col">Price</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Nike</td>
+                <td>忠孝館</td>
+                <td>8</td>
+                <td>$ 2700</td>
+              </tr>
+            </tbody>
+          </table>
         </div>
       </div>
     </>
