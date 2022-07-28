519fddd1d1307bb63ab2096326b3ed6459355522
Gary <garylin0969@gmail.com>
2022-07-28 21:49:14 +0800

    0728

--- src/pages/Admin/Admin.js
+++ src/pages/Admin/Admin.js
@@ -1,7 +1,33 @@
 import React from 'react';
+import './styles/Admin.scss';
 
 const Admin = () => {
-  return <div>Admin</div>;
+  return (
+    <>
+      <div className="w-100 vh-100 d-flex justify-content-end align-items-end">
+        <div className="work-area col-12 col-md-10 p-0 adminTable">
+          <table>
+            <thead>
+              <tr>
+                <th>sid</th>
+                <th>Name</th>
+                <th>Creat</th>
+                <th>Avatar</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>123</td>
+                <td>123</td>
+                <td>123</td>
+                <td>123</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Admin;
