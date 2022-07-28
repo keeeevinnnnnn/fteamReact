ea5e984ea9785155d1a330cbfbb78a2a0e8d9806
Gary <garylin0969@gmail.com>
2022-07-28 20:49:46 +0800

    0728

--- src/pages/Member/components/AllRecord/LessonCard.js
+++ src/pages/Member/components/AllRecord/LessonCard.js
@@ -23,19 +23,36 @@ const LessonCard = () => {
         });
     }
   }, [auth, token]);
-  console.log(lessonCard);
+  // 計算還剩幾天
+  // console.log(
+  //   Object.values(lessonCard).map((v, i) =>
+  //     parseInt((new Date(v.duringtime_end) - new Date()) / 1000 / 60 / 60 / 24)
+  //   )
+  // );
   return (
     <>
       {Object.values(lessonCard).map((v, i) => {
         return (
           <div className="m-2 lessonCard m-3" key={uuidv4()}>
             <div className="d-flex align-items-center text-center lessonhead">
-              <p className="col-4 m-0">{v.name}</p>
-              <p className="col-4 m-0">
+              <p className="col-3 m-0">{v.name}</p>
+              <p className="col-3 m-0">
                 <span>Begin</span> {v.duringtime_begin}
               </p>
-              <p className="col-4 m-0">
+              <p className="col-3 m-0">
                 <span>End</span> {v.duringtime_end}
+              </p>
+              <p className="col-3 m-0">
+                <span className="leftoverDaysNumber">
+                  {parseInt(
+                    (new Date(v.duringtime_end) - new Date()) /
+                      1000 /
+                      60 /
+                      60 /
+                      24
+                  )}
+                </span>
+                <span> Days</span>
               </p>
             </div>
             <div className="d-flex justify-content-center align-items-center lessonbody">
