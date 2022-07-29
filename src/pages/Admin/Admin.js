ce19c3a4a098d87471078bc8b0bb06a6ef788b1a
GaryL1n <106055223+GaryL1n@users.noreply.github.com>
2022-07-29 16:38:27 +0800

    0729

--- src/pages/Admin/Admin.js
+++ src/pages/Admin/Admin.js
@@ -17,34 +17,32 @@ const Admin = () => {
     <>
       <div className="w-100 vh-100 d-flex justify-content-end align-items-end">
         <div className="work-area col-12 col-md-10 p-0 adminTable">
-          <ScrollBox>
-            <table>
-              <thead>
-                <tr>
-                  <th>sid</th>
-                  <th>State</th>
-                  <th>Name</th>
-                  <th>Nickname</th>
-                  <th>Email</th>
-                  <th>Creat</th>
-                </tr>
-              </thead>
-              <tbody>
-                {allMember.map((v, i) => {
-                  return (
-                    <tr key={uuidv4()}>
-                      <td>{v.sid}</td>
-                      <td>{v.mem_bollen}</td>
-                      <td>{v.mem_name}</td>
-                      <td>{v.mem_nickname}</td>
-                      <td>{v.mem_email}</td>
-                      <td>{v.mem_created_at}</td>
-                    </tr>
-                  );
-                })}
-              </tbody>
-            </table>
-          </ScrollBox>
+          <table>
+            <thead>
+              <tr>
+                <th>sid</th>
+                <th>State</th>
+                <th>Name</th>
+                <th>Nickname</th>
+                <th>Email</th>
+                <th>Creat</th>
+              </tr>
+            </thead>
+            <tbody>
+              {allMember.map((v, i) => {
+                return (
+                  <tr key={uuidv4()}>
+                    <td>{v.sid}</td>
+                    <td>{v.mem_bollen}</td>
+                    <td>{v.mem_name}</td>
+                    <td>{v.mem_nickname}</td>
+                    <td>{v.mem_email}</td>
+                    <td>{v.mem_created_at}</td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
         </div>
       </div>
     </>
