8cf98383f7b94baa4c06351f58a03e5cd6c2986d
Gary <garylin0969@gmail.com>
2022-07-29 18:37:26 +0800

    0729

--- src/pages/Admin/Admin.js
+++ src/pages/Admin/Admin.js
@@ -17,32 +17,34 @@ const Admin = () => {
     <>
       <div className="w-100 vh-100 d-flex justify-content-end align-items-end">
         <div className="work-area col-12 col-md-10 p-0 adminTable">
-          <table>
-            <thead>
-              <tr>
-                <th>sid</th>
-                <th>State</th>
-                <th>Name</th>
-                <th>Nickname</th>
-                <th>Email</th>
-                <th>Creat</th>
-              </tr>
-            </thead>
-            <tbody>
-              {allMember.map((v, i) => {
-                return (
-                  <tr key={uuidv4()}>
-                    <td>{v.sid}</td>
-                    <td>{v.mem_bollen}</td>
-                    <td>{v.mem_name}</td>
-                    <td>{v.mem_nickname}</td>
-                    <td>{v.mem_email}</td>
-                    <td>{v.mem_created_at}</td>
-                  </tr>
-                );
-              })}
-            </tbody>
-          </table>
+          <div className="w-90 h-90">
+            <table className="h-100">
+              <thead>
+                <tr>
+                  <th>sid</th>
+                  <th>State</th>
+                  <th>Name</th>
+                  <th>Nickname</th>
+                  <th>Email</th>
+                  <th>Creat</th>
+                </tr>
+              </thead>
+              <tbody>
+                {allMember.map((v, i) => {
+                  return (
+                    <tr key={uuidv4()}>
+                      <td>{v.sid}</td>
+                      <td>{v.mem_bollen}</td>
+                      <td>{v.mem_name}</td>
+                      <td>{v.mem_nickname}</td>
+                      <td>{v.mem_email}</td>
+                      <td>{v.mem_created_at}</td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
+          </div>
         </div>
       </div>
     </>
