68334a5fe6e0c0ab8f9dea86a773763abcc0599d
GaryL1n <106055223+GaryL1n@users.noreply.github.com>
2022-07-29 16:09:46 +0800

    0728

--- src/pages/Admin/Admin.js
+++ src/pages/Admin/Admin.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { v4 as uuidv4 } from 'uuid';
 import './styles/Admin.scss';
+import ScrollBox from '../../components/ScrollBox/ScrollBox';
 
 const Admin = () => {
   const [allMember, setallMember] = useState([]);
@@ -16,32 +17,34 @@ const Admin = () => {
     <>
       <div className="w-100 vh-100 d-flex justify-content-end align-items-end">
         <div className="work-area col-12 col-md-10 p-0 adminTable">
-          <table>
-            <thead>
-              <tr>
-                <th>sid</th>
-                <th>State</th>
-                <th>Name</th>
-                <th>Nickname</th>
-                <th>Email</th>
-                <th>Creat</th>
-              </tr>
-            </thead>
-            <tbody>
-              {allMember.map((v, i) => {
-                return (
-                  <tr key={uuidv4()}>
-                    <td>{v.sid}</td>
-                    <td>{v.mem_bollen}</td>
-                    <td>{v.mem_name}</td>
-                    <td>{v.mem_nickname}</td>
-                    <td>{v.mem_email}</td>
-                    <td>{v.mem_created_at}</td>
-                  </tr>
-                );
-              })}
-            </tbody>
-          </table>
+          <ScrollBox>
+            <table>
+              <thead>
+                <tr>
+                  <th>sid</th>
+                  <th>State</th>
+                  <th>Name</th>
+                  <th>Nickname</th>
+                  <th>Email</th>
+                  <th>Creat</th>
+                </tr>
+              </thead>
+              <tbody>
+                {allMember.map((v, i) => {
+                  return (
+                    <tr key={uuidv4()}>
+                      <td>{v.sid}</td>
+                      <td>{v.mem_bollen}</td>
+                      <td>{v.mem_name}</td>
+                      <td>{v.mem_nickname}</td>
+                      <td>{v.mem_email}</td>
+                      <td>{v.mem_created_at}</td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
+          </ScrollBox>
         </div>
       </div>
     </>
