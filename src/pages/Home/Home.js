fb6e13c7f674302bbbabb036fd4e7c9bc72533ea
DEREK <es073727@yahoo.com.tw>
2022-08-14 21:21:32 +0800

    Homemb updata

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -6,8 +6,19 @@ const Home = () => {
   return (
     <>
       <div className="home-bg w-100 vh-100 d-flex justify-content-end align-items-end">
-        <div className="work-area col-10 pb-5 pe-5 text-danger">
+        <div className="work-area d-none d-md-block col-10 pb-5 pe-5 text-danger">
           <HomeSwiper />
+        </div>
+
+        <div className="work-area d-block d-md-none col-12 text-danger">
+          <div className="w-100 vh-100 d-flex justify-content-center align-items-center ">
+            <div className=" w-100 h-100 HomeMbo02"></div>
+          </div>
+          <div className="w-100 vh-100 HomeMbo03"></div>
+          <div className="w-100 vh-100 HomeMbo04"></div>
+          <div className="w-100 vh-100 HomeMbo05"></div>
+          <div className="w-100 vh-100 HomeMbo06"></div>
+          <div className="w-100 vh-100 HomeMbo07"></div>
         </div>
       </div>
     </>
