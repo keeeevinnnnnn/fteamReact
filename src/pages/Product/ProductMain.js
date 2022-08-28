15f597e3b98afec42ddc6bb217b1a0a0fbacdb29
kevin wu <kevin85627@gmail.com>
2022-08-28 20:10:29 +0800

    132

--- src/pages/Product/ProductMain.js
+++ src/pages/Product/ProductMain.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef, useContext } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import FilterBox from './components/FilterBox';
 import ToolBox from './components/ToolBox';
 import './styles/ProductMain.scss';
